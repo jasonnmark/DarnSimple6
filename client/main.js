3dcd8892c4c1e7145552234a62b9f67645534b49
Jason Mark <jason@gravityswitch.com>
2016-04-20 11:37:07 +0200

    rollback

--- client/main.js
+++ client/main.js
@@ -1,3 +1,1 @@
-/* not needed b/c of schema I'm guessing
 Meteor.subscribe('posts');
-*/
--- server/publications.js
+++ server/publications.js
@@ -1,10 +1,4 @@
-/*
 Meteor.publish('posts', function() {
   return Notes.find();
 });
-
-Meteor.publish('contacts', function() {
-  return Contacts.find();
-});
-*/
 

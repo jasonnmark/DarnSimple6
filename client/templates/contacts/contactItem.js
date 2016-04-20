9dc776f5523ce7929b4dabe9d86fe0769943903d
Jason Mark <jason@gravityswitch.com>
2016-04-20 12:05:33 +0200

    Refinements to add and list contacts

--- client/templates/contacts/contactItem.js
+++ client/templates/contacts/contactItem.js
@@ -1,10 +1,4 @@
 Template.contactItem.events({
-	"click .toggleIsItDone": function () {
-// 		console.log("done"); 
-	  	Contacts.update(this._id, {
-	  		$set: {isItDone: ! this.isItDone}
-	  	});
-	},
 	
 	'click .delete': function(e) {
 // 		console.log("done"); 
@@ -14,38 +8,7 @@ Template.contactItem.events({
 			Contacts.remove(currentContactId);
 			Router.go('dashboard');
 		}
-	},	
-
-	"click #edit": function(e, t) {
-// 	  console.log(this._id);
-      return Session.set("target" + t.data._id, true);
-      
-    },
-	
-	
-	'submit form': function(e) {
-	    e.preventDefault();
-	
-	    var currentContactId = this._id;
-	
-	    var contactProperties = {
-	      title: $(e.target).find('[name=title]').val()
-	    }
-	
-		// 	console.log(contactProperties); 
-		// 	console.log(this._id); 
-	
-	
-	
-	    Contacts.update(currentContactId, {$set: contactProperties}, function(error) {
-	      if (error) {
-	        // display the error to the user
-	        throwError(error.reason);
-	      } else {
-	        Router.go('dashboard', {_id: currentContactId});
-	      }
-	    });
-	},
+	}	
 	
 });
 

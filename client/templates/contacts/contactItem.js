648bea0e389d4479db8ceb3bc76e7d0fe5c7948c
Jason Mark <jason@gravityswitch.com>
2016-04-27 22:01:08 +0200

    nothing broken, but struggling with a couple different flavors of search.

--- client/templates/contacts/contactItem.js
+++ client/templates/contacts/contactItem.js
@@ -3,11 +3,11 @@ Template.contactItem.events({
 	'click .delete': function(e) {
 // 		console.log("done"); 
 		e.preventDefault();
-		if (confirm("Do you want to perminently delete this item? It can not be recovered?")) {
+// 		if (confirm("Do you want to perminently delete this item? It can not be recovered?")) {
 			var currentContactId = this._id;
 			Contacts.remove(currentContactId);
 			Router.go('dashboard');
-		}
+// 		}
 	},
 		
 	'click .contactStar': function () {

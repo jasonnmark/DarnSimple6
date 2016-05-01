2ae83f6ead11bc85c85669b8be2ad1c46171df47
Jason Mark <jason@gravityswitch.com>
2016-05-01 13:25:08 +0200

    WORKING: Auto search working. Results appearing based on keyup instead of submittal. All in one awkward window… but still it’s progress.

--- client/Templates/contacts/contactList.js
+++ client/Templates/contacts/contactList.js
@@ -1,7 +1,57 @@
+Template.contactList.onCreated(function contactListOnCreated() {
+  this.state = new ReactiveDict();
+});
+
+
 Template.contactList.helpers({
+  contactsIndex: () => ContactsIndex,
+	
+/*
   contacts: function() {
     return Contacts.find({}, {sort: {archived: 1, starred: -1, firstName: 1, lastName: 1}});
-  }
+*/
+
+  contacts() {
+   const instance = Template.instance();
+//  var searchString = instance.state.get('searchString');
+   if (instance.state.get('searchString')) {
+	  // return results based on what's typed so far
+	  
+      return Contacts.find({searchOnMe: instance.state.get('searchString')}, {sort: {archived: 1, starred: -1, firstName: 1, lastName: 1}});
+    }
+    // Otherwise, return starred contacts
+    return Contacts.find({}, {sort: {archived: 1, starred: -1, firstName: 1, lastName: 1}});
+	
+  },
+
+/*
+  notes() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Notes.find({ isItDone: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Notes.find({}, {sort: {dueDate: 1, starred: -1, title:1}});
+  },
+*/
+    
+    
+   
 });
 
 // 		var someCursor = Contacts.find({ firstName: searchString });
+
+/*
+Template.contactEasySearchBox.helpers({
+  contactsIndex: () => ContactsIndex
+});
+*/
+
+Template.contactList.events({
+	"keyup .selectize-input input" (event, instance) {
+    	instance.state.set('searchString', event.target.value);
+// 		console.log("change");
+		console.log(instance.state.get('searchString'));
+	},
+});
--- client/Templates/notes/noteList.js
+++ client/Templates/notes/noteList.js
@@ -1,5 +1,48 @@
+// Temporary UI state
+// https://www.meteor.com/tutorials/blaze/temporary-ui-state
+
 Template.noteList.helpers({
+/*
   notes: function() {
     return Notes.find({}, {sort: {dueDate: 1, starred: -1, title:1}});
   }
+*/
+  
+  notes() {
+    const instance = Template.instance();
+    if (instance.state.get('hideCompleted')) {
+      // If hide completed is checked, filter tasks
+      return Notes.find({ isItDone: { $ne: true } }, { sort: { createdAt: -1 } });
+    }
+    // Otherwise, return all of the tasks
+    return Notes.find({}, {sort: {dueDate: 1, starred: -1, title:1}});
+  },
+  
 });
+
+Template.noteList.onCreated(function noteListOnCreated() {
+  this.state = new ReactiveDict();
+});
+
+
+Template.noteList.events({
+	"change .hide-completed input" (event, instance) {
+    	instance.state.set('hideCompleted', event.target.checked);
+		console.log("click");
+		console.log(instance.state.get('hideCompleted'));
+
+	},
+	
+/*
+// Example from noteItem.js
+	
+	"click .toggleIsItDone": function () {
+	  	Notes.update(this._id, {
+	  		$set: {isItDone: ! this.isItDone}
+	  	});	
+	},
+*/
+
+});
+
+

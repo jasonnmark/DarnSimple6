8c284645fc52f8aa65ca98d3ce53fb7e6c8cd47a
Jason <jason@gravityswitch.com>
2016-05-22 12:20:01 +0200

    Trying avatar… if it doesn’t work it has to be removed.

---
+++ client/templates/application/avatar.js
@@ -0,0 +1,29 @@
+Avatar.options = {
+  fallbackType: 'initials',
+  gravatarDefault: 'identicon'
+  //defaultImageUrl: 'img/avatar.jpg'
+};
+
+Template.avatarTemplate.helpers({
+    users: function () {
+      return Meteor.users.find().fetch();
+    },
+    service: function (user) {
+      if      (user && user.services && user.services.twitter)   { return 'twitter'; }
+      else if (user && user.services && user.services.facebook)  { return 'facebook'; }
+      else if (user && user.services && user.services.google)    { return 'google'; }
+      else if (user && user.services && user.services.github)    { return 'github'; }
+      else if (user && user.services && user.services.instagram) { return 'instagram'; }
+      else                                                       { return 'none'; }
+    },
+    name: function (user) {
+      var name = '';
+      if (user && user.profile && user.profile.name) {
+        name = user.profile.name;
+      }
+      else if (user && user.username) {
+        name = user.username;
+      }
+      return name;
+    }
+  });

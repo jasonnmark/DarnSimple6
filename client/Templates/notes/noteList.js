450d4428e566c9b792185659a8bf1ecd407af9d1
Jason <jason@gravityswitch.com>
2016-05-23 10:26:13 +0200

    ###Stable but with some crud here and there. Trying to get search to work. Various cleanups. Checkin for Christine

--- client/Templates/notes/noteList.js
+++ client/Templates/notes/noteList.js
@@ -5,17 +5,50 @@
 
 Template.noteList.helpers({
   notes() {
-    const instance = Template.instance();
-    if (instance.state.get('hideCompleted')) {
-      // If hide completed is checked, filter tasks
-      return Notes.find({ isItDone: { $ne: true } }, { sort: { createdAt: -1 } });
+    var instance = Template.instance();
+    if (instance.state.get('showCompleted')) {
+	    return Notes.find({}, {sort: {dueDate: 1, starred: -1, title:1}});        	
     }
-    	// Otherwise, return all of the tasks
-    	return Notes.find({
-    }, {sort: {dueDate: 1, starred: -1, title:1}});
+    	// Otherwise, return just some
+			// If show completed is checked, filter tasks
+      return Notes.find(
+      	{ 
+	      	$or: [
+		      	{isItDone: { $ne: true } },
+		      	{title: "number"} 	
+	      	]
+	    	}, 
+	    	{ sort: { createdAt: -1 } }
+	    );
+
+
   },
 });
 
+/*
+// https://github.com/meteorhacks/search-source#get-the-reactive-data-source
+
+SearchSource.defineSource('packages', function(searchText, options) {
+  var options = {sort: {isoScore: -1}, limit: 20};
+
+  if(searchText) {
+    var regExp = buildRegExp(searchText);
+    var selector = {packageName: regExp, description: regExp};
+    return Packages.find(selector, options).fetch();
+  } else {
+    return Packages.find({}, options).fetch();
+  }
+});
+
+function buildRegExp(searchText) {
+  var words = searchText.trim().split(/[ \-\:]+/);
+  var exps = _.map(words, function(word) {
+    return "(?=.*" + word + ")";
+  });
+  var fullExp = exps.join('') + ".+";
+  return new RegExp(fullExp, "i");
+}
+*/
 
 
 
@@ -27,10 +60,10 @@ Template.noteList.onCreated(function noteListOnCreated() {
 
 
 Template.noteList.events({
-	"change .hide-completed input" (event, instance) {
-    	instance.state.set('hideCompleted', event.target.checked);
+	"change .show-completed input" (event, instance) {
+    	instance.state.set('showCompleted', event.target.checked);
 		console.log("click");
-		console.log(instance.state.get('hideCompleted'));
+		console.log(instance.state.get('showCompleted'));
 
 	},
 	
